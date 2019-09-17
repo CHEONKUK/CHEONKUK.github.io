bbcbedb39216fea86853cec4ed9fa65bb1beed5f
CHEONKUK <dd1139001@naver.com>
2019-09-17 23:03:58 +0900

    190917
    # NomadCode VanillaJs Study
       (momonton timer -> userName submit)

--- js/gretting.js
+++ js/gretting.js
@@ -6,6 +6,22 @@ const greeting = document.querySelector(".jsGreetings");
 const User = "currentUser";
 const showingClassName = "showing";
 
+function saveName(text) {
+  localStorage.setItem(User, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(showingClassName);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(showingClassName);
   greeting.classList.add(showingClassName);
@@ -16,6 +32,7 @@ function loadName() {
   const currentUser = localStorage.getItem(User);
   if (currentUser === null) {
     // he is not
+    askForName();
   } else {
     // he is
     paintGreeting(currentUser);
