5daf09589b1798a1973f3a1cbaaf93ebb8117427
CHEONKUK <dd1139001@naver.com>
2019-09-23 14:08:00 +0900

    190923
    # VanillaJS2 Study

---
+++ js/vanilla_js2_js/app.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  // 5:48 시작하기
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
