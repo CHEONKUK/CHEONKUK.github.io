9483bc0b22c7820ca001f2eedfe1e93b77d8232d
CHEONKUK <dd1139001@naver.com>
2019-10-15 14:46:12 +0900

    191015
    # React16 Study

--- react16/src/App.js
+++ react16/src/App.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <div>React16</div>;
+const BoundaryHOC = ProtectedComponent =>
+  class BoundaryHOC extends Component {
+    state = {
+      hasError: false
+    };
+
+    componentDidCatch = () => {
+      this.setState({
+        hasError: true
+      });
+    };
+
+    render() {
+      const { hasError } = this.setState;
+      if (hasError) {
+        return <ErrorFallback />;
+      } else {
+        return <ProtectedComponent />;
+      }
+    }
+  };
+
+const ErrorFallback = () => "Error ! ";
+
+class ErrorMaker extends Component {
+  state = {
+    name: ["A", "B", "C", "D"]
+  };
+
+  render() {
+    const { name } = this.state;
+    const names = name.map(name => <p>{name}</p>);
+
+    return (
+      <>
+        <div>{names}</div>
+      </>
+    );
+  }
 }
 
-export default App;
+const P_ErrorMaker = BoundaryHOC(ErrorMaker);
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div>React16</div>
+        <ErrorMaker />
+      </>
+    );
+  }
+}
+
+export default BoundaryHOC(App);
