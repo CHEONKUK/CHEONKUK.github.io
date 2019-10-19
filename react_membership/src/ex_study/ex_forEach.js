cbd62e98bdce6396de97e41a35aa7b70d807a909
CHEONKUK <dd1139001@naver.com>
2019-10-19 14:08:55 +0900

    191019
    # React Study

---
+++ react_membership/src/ex_study/ex_forEach.js
@@ -0,0 +1,9 @@
+let greeting = ["Hi", "Hello", "Bye"];
+
+// if (!greeting.includes("WOW")) {
+//   greeting.push("WOW!!!");
+// }
+
+greeting.forEach(grF => console.log(grF));
+greeting.map(grM => console.log(grM));
+// console.log(greeting);
